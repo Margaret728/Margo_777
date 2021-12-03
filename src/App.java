a8fab49470398dc5db5c2b4a33b296c1519a3575
Margaret728 <90791651+Margaret728@users.noreply.github.com>
2021-12-02 21:24:52 -0600

    This is my first commit of a tool that draws a filled circle or square.

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+	public static void main(String[] args) {
+		// opens the draw choice frame and shows a circle initially
+		DrawChoiceFrame dcf = new DrawChoiceFrame();
+		dcf.setVisible(true);
+	}
+}
---
+++ src/DrawChoiceFrame.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class DrawChoiceFrame extends JFrame {
+	private DrawChoicePanel dcp;
+	public void setupGUI() {
+		setTitle("Draw Choice Frame V1.0");
+		setBounds(100,100,300,300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		JPanel panSouth = new JPanel();
+		panSouth.setLayout(new FlowLayout());
+		JButton btnCircle = new JButton("Circle");
+		panSouth.add(btnCircle);
+		btnCircle.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dcp.setShapeType(0);
+						repaint();  // frame repaint --> all LW components to paintComponent themselves
+					}
+				}
+		);
+		JButton btnSquare = new JButton("Square");
+		panSouth.add(btnSquare);
+		btnSquare.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dcp.setShapeType(1);
+						repaint();  // frame repaint --> all LW components to paintComponent themselves
+					}
+				}
+		);
+		c.add(panSouth,BorderLayout.SOUTH);
+		dcp = new DrawChoicePanel();
+		c.add(dcp,BorderLayout.CENTER);
+	}
+	public DrawChoiceFrame() {
+		setupGUI();
+	}
+}
