a2ffa409424fc2fbc16ff30ebc9bf9de62249e5a
Margaret728 <90791651+Margaret728@users.noreply.github.com>
2021-12-02 21:37:43 -0600

    Added a comment to the main file.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
 	public static void main(String[] args) {
 		// opens the draw choice frame and shows a circle initially
+		//My first GIT
 		DrawChoiceFrame dcf = new DrawChoiceFrame();
 		dcf.setVisible(true);
 	}
